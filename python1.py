d4dd5e78a153d563418294d9c59a5aab720a3976
jayakrishna <157739427+jayakrishna15@users.noreply.github.com>
2024-02-10 12:01:04 +0530

    python programs

---
+++ python1.py
@@ -0,0 +1,89 @@
+print("this is my first python")
+
+
+#string concatination
+print("jaya"+" "+"krishna")
+print("hari"+" "+"icon")
+print("g"+" "+"ganesh")
+print("jaya"+" "+"krishna")
+print("shaja"+""+"han")
+print("virat"+" "+"kohli")
+
+
+#escape a string
+#example:
+print("1.this my \"first\" python program \n 2.this is second line in python")
+
+print("3.rohit sharma is great six \"hitter\" in the world \n 4.ms.dhoni is also one of the big six hitter in the cricket history \n 5.practies is a very important")
+
+#f-string
+#example:
+hi = "ilove you"
+print(f"*  hello baby {hi} ")
+ #example2:
+hello = "i want learn a"
+print(f"*  iam new to python but {hello} python")
+#example3:
+am = "to \"learn\""
+print(f"it's very tough {am} what do you say")
+#example4:
+A = "\"mitrama\""
+print(f"hello {A} how are you man is everything ok")
+B = "\"ms.dhoni\""
+print(f"one and only man in the cricket history in the world single handedly won a world cup for team india that person name is {B} ")
+
+
+
+
+
+#converting data types
+rupees = 1.5
+rupeesint = int(rupees)
+print("*  changing float value to int",rupeesint)
+
+#example2:
+intnumbers = 55
+intnumbersfloat = float(intnumbers)
+print("*  iam changing int to float",intnumbersfloat)
+
+
+
+#num string
+#example of num string:
+
+numstr="50"
+print(float(numstr))
+
+#example3:
+numstr = "500"
+print(int(numstr))
+
+
+#checking data types:
+i = 51.021
+print(type(i))
+
+#example2:
+a = 545
+print(type(a))
+
+
+#example3:
+d = (20.3)
+print(type(d))
+
+#example:4
+c = (20)
+print(int(c))
+
+numstr=500
+print(float(numstr))
+
+numstr=500.99
+print(int(numstr))
+
+ 
+
+  
+
+
